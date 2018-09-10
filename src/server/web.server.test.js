fc01dac6daf47fc8d7ea8bd7861f0a4fcbaa4ef6
Ma7eer <maher.alkendi2@gmail.com>
2018-09-10 07:43:38 -0600

    added testing using jest

---
+++ src/server/web.server.test.js
@@ -0,0 +1,17 @@
+import ExpressWebServer from './web.server'
+
+describe('Started', () => {
+  let webServer = null
+  beforeAll(() => {
+    webServer = new ExpressWebServer()
+  })
+
+  test('should start and trigger a callback', async () => {
+    let promise = webServer.start();
+    await expect(promise).resolves.toBeUndefined();
+  })
+  test('should stop and trigger a callback', async () => {
+    let promise = webServer.stop();
+    await expect(promise).resolves.toBeUndefined();
+  })
+})
