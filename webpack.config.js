36719d33ff38fdee1fce0345f3378fbc05006c50
Ma7eer <maher.alkendi2@gmail.com>
2018-09-11 08:13:08 -0600

    added caching

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,62 @@
+let webpack = require('webpack');
 let path = require('path');
 let nodeExternals = require('webpack-node-externals');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 let HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const moduleObj = {
-    rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: "babel-loader"
-        }
-    }],
+  rules: [{
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader'
+    }
+  }],
 };
 
 const client = {
-    entry: {
-      'client': './src/client/index.js'
-    },
-    target: 'web',
-    output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, 'dist/public')
-    },
-    module: moduleObj,
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: 'src/client/index.html'
-      })
-    ]
-  }
+  entry: {
+    'client': './src/client/index.js'
+  },
+  target: 'web',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist/public')
+  },
+  module: moduleObj,
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebPackPlugin({
+      template: 'src/client/index.html',
+      title: 'Caching'
+    }),
+    new webpack.HashedModuleIdsPlugin()
+  ]
+};
 
 const server = {
-    entry: {
-        'server': './src/server/index.js'
-    },
-    target: 'node',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: moduleObj,
-    externals: [nodeExternals()]
-}
+  entry: {
+    'server': './src/server/index.js'
+  },
+  target: 'node',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: moduleObj,
+  externals: [nodeExternals()]
+};
 
 module.exports = [client, server];
